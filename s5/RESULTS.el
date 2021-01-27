3c76e38c4220559dfc4fb62e9601017d12edcdcc
Matthew Wiesner <wiesner@jhu.edu>
2021-01-27 13:15:48 -0500

    Adds greek results

---
+++ s5/RESULTS.el
@@ -0,0 +1,4 @@
+%WER 28.55 [ 5290 / 18527, 359 ins, 2050 del, 2881 sub ] exp/chain/cnn_tdnn1c_sp/decode_valid_sentence/wer_11_0.0
+%WER 25.03 [ 4296 / 17166, 332 ins, 1204 del, 2760 sub ] exp/chain/cnn_tdnn1c_sp/decode_eval_sentence/wer_10_0.0
+%WER 31.63 [ 5858 / 18519, 557 ins, 2153 del, 3148 sub ] exp/chain/cnn_tdnn1c_sp/decode_valid/wer_11_0.0
+%WER 27.05 [ 4640 / 17153, 456 ins, 1317 del, 2867 sub ] exp/chain/cnn_tdnn1c_sp/decode_eval/wer_10_0.0
